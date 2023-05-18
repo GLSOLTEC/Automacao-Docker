d44add5783d68b437b1ad8c7c6292b1ce676b0be
Pascoal Freitas <pstephanine@gmail.com>
2023-05-18 00:00:23 -0300

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -9,7 +9,7 @@ echo "###INSTALAÇÃO DOCKER ###"
 apt purge docker-* -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo apt install docker-compose -y
+sudo apt install build-essential docker-compose -y
 cd ../
 git clone https://$login:$token@github.com/mmarinhols/rmserver.git
 git clone https://$login:$token@github.com/mmarinhols/rmwebhook.git
